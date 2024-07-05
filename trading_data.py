a42dec16d79dcc72f75b89c54561a347b379647d
Michael Thinh <michaelthinh1008@gmail.com>
2024-07-05 15:52:53 +0700

    fix: cannot get item

--- trading_data.py
+++ trading_data.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from constant import coins, timeframes
 
 def getDataFromCoin(coin, timeframe, day_number):
   url = f"https://www.bitstamp.net/api/v2/ohlc/{coin}/"
@@ -7,32 +8,22 @@ def getDataFromCoin(coin, timeframe, day_number):
           "step":timeframe,
           "limit":int(day_number),
           }
-  response = requests.get(url, params=params).json()
-  data = response.get("data", [])
-  ohlc = data.get("ohlc", [])
-  df = pd.DataFrame(ohlc, columns=["timestamp", "open", "high", "low", "close"])
-  df['timestamp'] = pd.to_datetime(df['timestamp'], unit = "s")
+  df = requests.get(url, params=params).json()["data"]["ohlc"]
+
+  df = pd.DataFrame(df)
+  df.timestamp = pd.to_datetime(df.timestamp, unit = "s")
+  df.open = df.open.astype(float)
+  df.close = df.close.astype(float)
+  df.high = df.high.astype(float)
+  df.low = df.low.astype(float)
   return df
 
 def getDataFromCoinToCSV(coin):
-    url = f"https://www.bitstamp.net/api/v2/ohlc/{coin}/"
-    params = {
-        "step": 86400,
-        "limit": int(365*5),
-    }
-    response = requests.get(url, params=params).json()
-    if 'data' in response:
-        df = pd.DataFrame(response["data"]["ohlc"])
-    else:
-        print(f"Error: No data found for {coin}")
-        return
-    df.timestamp = pd.to_datetime(df.timestamp, unit = "s")
-    new_df = df[['timestamp', 'open', 'close', 'high', 'low']]
-    new_df.to_csv(f'./data/{coin}.csv')
+  df = getDataFromCoin(coin, timeframes['day']['value'], 1000)
+  new_df = df[['timestamp', 'open', 'close', 'high', 'low']]
+  new_df.to_csv(f'./data/{coin}.csv')
 
 def getAllDataToCSV():
-  coins = [
-    'btcusd', 'ethusd', 'xrpusd', 'ltcusd', 'adausd', 'dotusd', 'solusd', 'linkusd', 'maticusd', 'dogeusd', ]
   for coin in coins:
     getDataFromCoinToCSV(coin)
 
